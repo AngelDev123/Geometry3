35e4e04c57881607dfbf6cee35a21c164df2d447
Dev_AnGeL <kill-20-20@mail.com>
2019-04-28 19:04:02 +0700

    Write input function

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+
+int main()
+{
+	float x1, y1, r1, x2, y2, r2, r;
+	float dist;
+
+	printf("Circle #1: ");
+	scanf("%f%f%f", &x1, &y1, &r1);
+
+	printf("\n\nCircle #2: ");
+	scanf("%f%f%f", &x2, &y2, &r2);
+
+	
+}
